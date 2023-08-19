115109902d4da1619d4f96fc010d9f6ee20950c8
Wealthineer <hi@wealthineer.com>
2023-08-19 10:52:18 +0200

    Geenrated the types for the contract ABIs

---
+++ wagmi.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@wagmi/cli'
+
+
+import { erc721MembershipMintAbi } from './constants/abis'
+import { usdcMockAbi } from './constants/abis'
+
+// @ts-ignore
+export default defineConfig({
+  out: 'src/generated.ts',
+  contracts: [{
+    name: "Erc721MembershipMint.sol",
+    abi: erc721MembershipMintAbi
+  },
+  {
+    name: "UsdcMock.sol",
+    abi: usdcMockAbi
+  }]
+})
